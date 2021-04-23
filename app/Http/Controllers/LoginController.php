e566d80d181641f98afaa318a748e218bd05b0d2
Lucas1024b <lucasribeiro1024b@gmail.com>
2021-04-23 15:29:06 -0300

    Fixed issue #1

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,34 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
     public function authenticate(Request $request)
     {
-        /*
-        $email = $request->email;
-        $senha = $request->senha;
-
-        $credentials = $request->only($email, $senha);
-
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-
-            return redirect()->intended('livros');
-        }
-
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ]);
-        */
-        if (auth()->attempt(request(['email', 'password'])) == false) {
-            return back()->withErrors([
-                'message' => 'The email or password is incorrect, please try again'
+        if (! Auth::attempt(request(['email', 'password']))) {
+            throw ValidationException::withMessages([
+                'email' => __('auth.failed'),
             ]);
         }
 
+        $request->session()->regenerate();
+
         return redirect()->intended('livros');
+
     }
 
     public function createView() {
@@ -40,24 +29,18 @@ class LoginController extends Controller
     }
 
     public function create(Request $request) {
-       /*
-        $user = new User();
-        $user->name = $request->nome;
-        $user->email = $request->email;
-        $user->password = $request->senha;
-        $user->save();
 
-        Auth::login($user);
-
-        return redirect()->route('livros');
-    */
         $this->validate(request(), [
             'name' => 'required',
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
-        $user = User::create(request(['name', 'email', 'password']));
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
 
         Auth::login($user);
         return redirect()->route('livros');
