aec895bb2eaceea22756b8a1fe53727d37d79bbe
Lucas1024b <lucasribeiro1024b@gmail.com>
2021-04-28 15:38:44 -0300

    #2 - rwork: corrigindo uso da PSR-PEC-2

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -20,7 +20,7 @@ class LoginController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('view/livro/livro-view');
+        return redirect()->intended('view/livros');
 
     }
 
